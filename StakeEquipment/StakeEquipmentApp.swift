d64d698e836ba18eceb097cdba82f9394a3794a3
keshew <artyom.korotckov2017@yandex.ru>
2025-06-26 13:02:32 +0500

    Initial Commit

---
+++ StakeEquipment/StakeEquipmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  StakeEquipmentApp.swift
+//  StakeEquipment
+//
+//  Created by Артём Коротков on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct StakeEquipmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
